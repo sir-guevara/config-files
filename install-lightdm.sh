7555a6e4bf92dc98f750c485da9c15063c13d9df
Sir Guevara <groovyguev@gmail.com>
2026-07-27 17:15:17 -0500

    new fixes to notes and added new features

--- install-lightdm.sh
+++ install-lightdm.sh
@@ -2,7 +2,7 @@
 set -eu
 
 repo_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-theme=Gruvbox-B-MB-Light-Soft
+theme=Groovy-LightDM
 icons=Gruvbox-Dark
 desktop_user=${SUDO_USER:-${USER:-}}
 desktop_home=$(getent passwd "$desktop_user" | cut -d: -f6)
@@ -12,11 +12,12 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 install -d /usr/share/themes /usr/share/icons /usr/share/backgrounds /etc/lightdm
-test -d "$desktop_home/.local/share/themes/$theme"
 test -d "$desktop_home/.local/share/icons/$icons"
-cp -a "$desktop_home/.local/share/themes/$theme" /usr/share/themes/
+install -d "/usr/share/themes/$theme/gtk-3.0"
+install -m 0644 "$repo_dir/lightdm/$theme/gtk-3.0/gtk.css" \
+    "/usr/share/themes/$theme/gtk-3.0/gtk.css"
 cp -a "$desktop_home/.local/share/icons/$icons" /usr/share/icons/
-install -m 0644 "$desktop_home/walls/wall10.png" /usr/share/backgrounds/groovy-lightdm.jpg
+install -m 0644 "$desktop_home/walls/wall9.jpg" /usr/share/backgrounds/groovy-lightdm.png
 
 if [ -f /etc/lightdm/lightdm-gtk-greeter.conf ] && \
    [ ! -f /etc/lightdm/lightdm-gtk-greeter.conf.groovy-backup ]; then
